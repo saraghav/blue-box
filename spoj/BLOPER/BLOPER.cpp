8384c36aeabbf6d2359b8c01534349d92eff3c3b
saraghav <araghav.s@gmail.com>
2016-08-27 19:15:12 -0500

    modifications to a cleaner version of BLOPER. still not AC though

--- spoj/BLOPER/BLOPER.cpp
+++ spoj/BLOPER/BLOPER.cpp
@@ -5,54 +5,39 @@
 #include <vector>
 using namespace std;
 
-string get_expression(int const& N, int const& S);
-string form_expression(vector<char> const& sign);
+string const get_expression(int const& N, int const& S);
+string const form_expression(vector<char> const& sign);
 
 int main(int argc, char *argv[]) {
     int N, S;
     scanf("%d %d", &N, &S);
 
-    string result = get_expression(N, S);
-    cout << result;
+    string const result = get_expression(N, S);
+    cout << result << endl;
 
     return 0;
 }
 
-string get_expression(int const& N, int const& S) {
+string const get_expression(int const& N, int const& S) {
     string const impossible = "Impossible";
 
-    vector<bool> num_taken(N+1, false);
-    num_taken[0] = true; // does not exist
-    num_taken[1] = true; // always positive
     vector<char> sign(N+1, '+');
     sign[0] = '\0'; // does not exist
     sign[1] = '\0'; // always positive
 
     int sum = N*(N+1) / 2;
+    int desired = (sum-S)/2;
 
-    int numbers_left = N-1;
-    while (numbers_left > 0) {
-        if (sum > S) {
-            int desired = abs( (sum-S)/2 );
-            if (desired > N) {
-                desired = N;
-            }
-            while (desired > 1) {
-                if (!num_taken[desired]) {
-                    sum -= 2*desired;
-                    num_taken[desired] = true;
-                    sign[desired] = '-';
-                    numbers_left--;
-                    break;
-                }
-                desired--;
-            }
-
-            if (desired <= 1) {
-                break;
-            }
-        } else {
+    for (int current_num=N; current_num>1; current_num--) {
+        if (desired <= 1) {
             break;
+        } else {
+            // because 1 cannot change sign, the smallest pair can only be 2+some_number = desired
+            if (current_num == desired || current_num <= (desired-2)) {
+                sum -= 2*current_num;
+                sign[current_num] = '-';
+                desired = (sum-S)/2;
+            }
         }
     }
 
@@ -63,7 +48,7 @@ string get_expression(int const& N, int const& S) {
     }
 }
 
-string form_expression(vector<char> const& sign) {
+string const form_expression(vector<char> const& sign) {
     string expression = "";
     for (int i=1; i<sign.size(); i++) {
         expression += (sign[i] + to_string(i));
